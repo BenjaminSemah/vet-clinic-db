6ad388b7f8068906ce3cd6b6a665def3e30c2a8a
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-02 05:32:29 +0000

    Add queries to answer project #4 questions

--- data.sql
+++ data.sql
@@ -106,4 +106,3 @@ VALUES((SELECT id from animals where name='Squirtle'),(SELECT id from vets where
 ((SELECT id from animals where name='Boarmon'),(SELECT id from vets where name='Maisy Smith'),'2020-08-03'),
 ((SELECT id from animals where name='Blossom'),(SELECT id from vets where name='Stephanie Mendez'),'2020-05-24'),
 ((SELECT id from animals where name='Blossom'),(SELECT id from vets where name='William Tatcher'),'2021-01-11');
-
